b7fae20e9568e2784d334c088fbe750714739f35
Michellesetoboli <mesetoboli@gmail.com>
2022-10-19 11:30:24 -0700

    cleaned up files and changed main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
  */
 void print_alphabet(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -20,20 +20,24 @@ int main(void)
 
 
 
-	        r = _islower('H');
+	        r = _isalpha('H');
 
 		    _putchar(r + '0');
 
-		        r = _islower('o');
+		        r = _isalpha('o');
 
 			    _putchar(r + '0');
 
-			        r = _islower(108);
+			        r = _isalpha(108);
 
 				    _putchar(r + '0');
 
-				        _putchar('\n');
+				        r = _isalpha(';');
 
-					    return (0);
+					    _putchar(r + '0');
+
+					        _putchar('\n');
+
+						    return (0);
 
 }
