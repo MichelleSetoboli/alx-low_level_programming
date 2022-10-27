e577ed1685d895b2764d16af254574e57ab88a4f
Michellesetoboli <mesetoboli@gmail.com>
2022-10-27 03:01:43 -0700

    A function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
   * @n: input value
   * Return: dest
   */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
@@ -24,4 +24,3 @@ char *_strncpy(char *dest, char *src, int n);
 	}
 	return (dest);
 }
-	
